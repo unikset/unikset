625f9723ae2555551a170e286ad7d171a9294df3
admin <foreach@mail.ru>
2012-12-26 20:17:25 +0200

    26.12

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/YiiFramework/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+if($_SERVER['SERVER_ADDR']==='127.0.0.1')
+{
+    $config=dirname(__FILE__).'/protected/config/main_loc.php';
+}
+else
+{
+    $config=dirname(__FILE__).'/protected/config/main.php';
+}
+
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
